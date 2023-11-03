051b7fee329daa1dcfd86248849d679a5b971877
Alexander <voprosy262@gmail.com>
2023-11-04 00:12:27 +0300

    refactor: convert Planet component to functional

--- src/components/cards/Planet/Planet.tsx
+++ src/components/cards/Planet/Planet.tsx
@@ -1,116 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import styles from './Planet.module.scss';
 import Loader from '../../Loader/Loader';
-import withRouter from '../../../utils/withRouter';
-import { IPlanetState, IPlanetData, RouterProps } from '../../../types/types';
+import { IPlanetData } from '../../../types/types';
 import getPlanetData from '../../../services/api/getPlanetData';
 import AttributesBlock from '../../AttributesBlock/AttributesBlock';
 import hasNoData from '../../../services/hasNoData';
 
-class Planet extends React.Component<RouterProps, IPlanetState> {
-  constructor(props: RouterProps) {
-    super(props);
+const Planet: React.FC = () => {
+  const params = useParams();
+  const [planetData, setPlanetData] = useState<null | IPlanetData>(null);
 
-    this.state = {
-      planetData: null,
-    };
-  }
-
-  async setInitState() {
-    await this.setState({
-      planetData: null,
-    });
-  }
-
-  getPlanetData = async (id: string) => {
+  const fetchPlanetData = async (id: string) => {
     const planetData: IPlanetData = await getPlanetData(id);
-    await this.setState({ planetData });
+    await setPlanetData(planetData);
   };
 
-  componentDidMount = async (): Promise<void> => {
-    await this.getPlanetData(this.props.params.id as string);
-  };
+  useEffect(() => {
+    fetchPlanetData(params.id as string);
+  }, [params.id]);
 
-  componentDidUpdate = async (prevProps: RouterProps) => {
-    if (prevProps.location.pathname !== this.props.location.pathname) {
-      this.setInitState();
-      await this.getPlanetData(this.props.params.id as string);
-    }
-  };
-
-  render() {
-    if (!this.state.planetData) {
-      return <Loader />;
-    }
-
-    if (this.state.planetData) {
-      const planetId: string = this.state.planetData.url.replace(/[^0-9]/g, '');
-      const planetImgSrc: string = `/images/planets/${planetId}.jpg`;
-
-      return (
-        <div className={styles.item__wrapper}>
-          <div className={styles.item__container}>
-            <div className={styles.item__container_left}>
-              <h1 className={styles.item__title}>{this.state.planetData.name}</h1>
-              <div>
-                {hasNoData(this.state.planetData.climate) || (
-                  <p>Climate: {this.state.planetData.climate}</p>
-                )}
-                {hasNoData(this.state.planetData.terrain) || (
-                  <p>Terrain: {this.state.planetData.terrain}</p>
-                )}
-                {hasNoData(this.state.planetData.diameter) || (
-                  <p>Diameter: {Number(this.state.planetData.diameter).toLocaleString()} km</p>
-                )}
-                {hasNoData(this.state.planetData.gravity) || (
-                  <p>Gravity: {this.state.planetData.gravity}</p>
-                )}
-                {hasNoData(this.state.planetData.rotation_period) || (
-                  <p>Rotation period: {this.state.planetData.rotation_period} hours</p>
-                )}
-                {hasNoData(this.state.planetData.orbital_period) || (
-                  <p>Orbital period: {this.state.planetData.orbital_period} days</p>
-                )}
-                {hasNoData(this.state.planetData.surface_water) || (
-                  <p>Water coverage: {this.state.planetData.surface_water}%</p>
-                )}
-                {hasNoData(this.state.planetData.population) || (
-                  <p>Population: {Number(this.state.planetData.population).toLocaleString()}</p>
-                )}
-              </div>
-              <div className={styles.attributes_container}>
-                {!!this.state.planetData.films.length && (
-                  <AttributesBlock
-                    data={this.state.planetData.films}
-                    classNames={['item__link']}
-                    title="Films"
-                  />
-                )}
-                {!!this.state.planetData.residents.length && (
-                  <AttributesBlock
-                    data={this.state.planetData.residents}
-                    classNames={['item__link']}
-                    title="Residents"
-                  />
-                )}
-              </div>
-            </div>
-            <img
-              className={styles.item__img}
-              onError={({ currentTarget }) => {
-                console.clear();
-                currentTarget.onerror = null;
-                currentTarget.src = '/images/png/img_not_found.png';
-                currentTarget.style.width = '400px';
-              }}
-              alt={this.state.planetData.name}
-              src={planetImgSrc}
-            />
-          </div>
-        </div>
-      );
-    }
+  if (!planetData) {
+    return <Loader />;
   }
-}
 
-export default withRouter<RouterProps>(Planet);
+  if (planetData) {
+    const planetId: string = planetData.url.replace(/[^0-9]/g, '');
+    const planetImgSrc: string = `/images/planets/${planetId}.jpg`;
+
+    return (
+      <div className={styles.item__wrapper}>
+        <div className={styles.item__container}>
+          <div className={styles.item__container_left}>
+            <h1 className={styles.item__title}>{planetData.name}</h1>
+            <div>
+              {hasNoData(planetData.climate) || <p>Climate: {planetData.climate}</p>}
+              {hasNoData(planetData.terrain) || <p>Terrain: {planetData.terrain}</p>}
+              {hasNoData(planetData.diameter) || (
+                <p>Diameter: {Number(planetData.diameter).toLocaleString()} km</p>
+              )}
+              {hasNoData(planetData.gravity) || <p>Gravity: {planetData.gravity}</p>}
+              {hasNoData(planetData.rotation_period) || (
+                <p>Rotation period: {planetData.rotation_period} hours</p>
+              )}
+              {hasNoData(planetData.orbital_period) || (
+                <p>Orbital period: {planetData.orbital_period} days</p>
+              )}
+              {hasNoData(planetData.surface_water) || (
+                <p>Water coverage: {planetData.surface_water}%</p>
+              )}
+              {hasNoData(planetData.population) || (
+                <p>Population: {Number(planetData.population).toLocaleString()}</p>
+              )}
+            </div>
+            <div className={styles.attributes_container}>
+              {!!planetData.films.length && (
+                <AttributesBlock
+                  data={planetData.films}
+                  classNames={['item__link']}
+                  title="Films"
+                />
+              )}
+              {!!planetData.residents.length && (
+                <AttributesBlock
+                  data={planetData.residents}
+                  classNames={['item__link']}
+                  title="Residents"
+                />
+              )}
+            </div>
+          </div>
+          <img
+            className={styles.item__img}
+            onError={({ currentTarget }) => {
+              console.clear();
+              currentTarget.onerror = null;
+              currentTarget.src = '/images/png/img_not_found.png';
+              currentTarget.style.width = '400px';
+            }}
+            alt={planetData.name}
+            src={planetImgSrc}
+          />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Planet;
