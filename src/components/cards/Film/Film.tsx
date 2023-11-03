5f6b627434a4794c06d4a2552d930df4adfafa45
Alexander <voprosy262@gmail.com>
2023-11-04 01:23:22 +0300

    refactor: convert Film component to functional

--- src/components/cards/Film/Film.tsx
+++ src/components/cards/Film/Film.tsx
@@ -1,128 +1,104 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import styles from './Film.module.scss';
 import Loader from '../../Loader/Loader';
-import withRouter from '../../../utils/withRouter';
-import { IFilmState, IFilmData, RouterProps } from '../../../types/types';
+import { IFilmData } from '../../../types/types';
 import getFilmData from '../../../services/api/getFilmData';
 import hasNoData from '../../../services/hasNoData';
 import dateFormat from '../../../services/dateFormat';
 import AttributesBlock from '../../AttributesBlock/AttributesBlock';
 
-class Film extends React.Component<RouterProps, IFilmState> {
-  constructor(props: RouterProps) {
-    super(props);
+const Film: React.FC = () => {
+  const params = useParams();
+  const [filmData, setFilmData] = useState<null | IFilmData>(null);
 
-    this.state = {
-      filmData: null,
-    };
-  }
-
-  async setInitState() {
-    await this.setState({
-      filmData: null,
-    });
-  }
-
-  getFilmData = async (id: string) => {
+  const fetchFilmData = async (id: string) => {
     const filmData: IFilmData = await getFilmData(id);
-    await this.setState({ filmData });
+    await setFilmData(filmData);
   };
 
-  componentDidMount = async (): Promise<void> => {
-    await this.getFilmData(this.props.params.id as string);
-  };
+  useEffect(() => {
+    fetchFilmData(params.id as string);
+  }, [params.id]);
 
-  componentDidUpdate = async (prevProps: RouterProps) => {
-    if (prevProps.location.pathname !== this.props.location.pathname) {
-      this.setInitState();
-      await this.getFilmData(this.props.params.id as string);
-    }
-  };
+  if (!filmData) {
+    return <Loader />;
+  }
 
-  render() {
-    if (!this.state.filmData) {
-      return <Loader />;
-    }
+  if (filmData) {
+    const filmId: string = filmData.url.replace(/[^0-9]/g, '');
+    const filmImgSrc: string = `/images/films/${filmId}.jpg`;
 
-    if (this.state.filmData) {
-      const filmId: string = this.state.filmData.url.replace(/[^0-9]/g, '');
-      const filmImgSrc: string = `/images/films/${filmId}.jpg`;
-
-      return (
-        <div className={styles.item__wrapper}>
-          <div className={styles.item__container}>
-            <div className={styles.item__wrapper_description}>
-              <div className={styles.item__container_left}>
-                <h1 className={styles.item__title}>{this.state.filmData.title}</h1>
-                <div>
-                  {hasNoData(this.state.filmData.opening_crawl) || (
-                    <p className={styles.description}>{this.state.filmData.opening_crawl}</p>
-                  )}
-                  {hasNoData(this.state.filmData.director) || (
-                    <p>Director: {this.state.filmData.director}</p>
-                  )}
-                  {hasNoData(this.state.filmData.producer) || (
-                    <p>Producer: {this.state.filmData.producer}</p>
-                  )}
-                  {hasNoData(this.state.filmData.release_date) || (
-                    <p>Release date: {dateFormat(this.state.filmData.release_date)}</p>
-                  )}
-                </div>
+    return (
+      <div className={styles.item__wrapper}>
+        <div className={styles.item__container}>
+          <div className={styles.item__wrapper_description}>
+            <div className={styles.item__container_left}>
+              <h1 className={styles.item__title}>{filmData.title}</h1>
+              <div>
+                {hasNoData(filmData.opening_crawl) || (
+                  <p className={styles.description}>{filmData.opening_crawl}</p>
+                )}
+                {hasNoData(filmData.director) || <p>Director: {filmData.director}</p>}
+                {hasNoData(filmData.producer) || <p>Producer: {filmData.producer}</p>}
+                {hasNoData(filmData.release_date) || (
+                  <p>Release date: {dateFormat(filmData.release_date)}</p>
+                )}
               </div>
-              <img
-                className={styles.item__img}
-                onError={({ currentTarget }) => {
-                  console.clear();
-                  currentTarget.onerror = null;
-                  currentTarget.src = '/images/png/img_not_found.png';
-                  currentTarget.style.width = '400px';
-                }}
-                alt={this.state.filmData.title}
-                src={filmImgSrc}
+            </div>
+            <img
+              className={styles.item__img}
+              onError={({ currentTarget }) => {
+                console.clear();
+                currentTarget.onerror = null;
+                currentTarget.src = '/images/png/img_not_found.png';
+                currentTarget.style.width = '400px';
+              }}
+              alt={filmData.title}
+              src={filmImgSrc}
+            />
+          </div>
+          <div className={styles.attributes_container}>
+            {!!filmData.characters.length && (
+              <AttributesBlock
+                data={filmData.characters}
+                classNames={['item__link']}
+                title="Characters"
               />
-            </div>
-            <div className={styles.attributes_container}>
-              {!!this.state.filmData.characters.length && (
-                <AttributesBlock
-                  data={this.state.filmData.characters}
-                  classNames={['item__link']}
-                  title="Characters"
-                />
-              )}
-              {!!this.state.filmData.planets.length && (
-                <AttributesBlock
-                  data={this.state.filmData.planets}
-                  classNames={['item__link']}
-                  title="Planets"
-                />
-              )}
-              {!!this.state.filmData.starships.length && (
-                <AttributesBlock
-                  data={this.state.filmData.starships}
-                  classNames={['item__link']}
-                  title="Starships"
-                />
-              )}
-              {!!this.state.filmData.vehicles.length && (
-                <AttributesBlock
-                  data={this.state.filmData.vehicles}
-                  classNames={['item__link']}
-                  title="Vehicles"
-                />
-              )}
-              {!!this.state.filmData.species.length && (
-                <AttributesBlock
-                  data={this.state.filmData.species}
-                  classNames={['item__link']}
-                  title="Species"
-                />
-              )}
-            </div>
+            )}
+            {!!filmData.planets.length && (
+              <AttributesBlock
+                data={filmData.planets}
+                classNames={['item__link']}
+                title="Planets"
+              />
+            )}
+            {!!filmData.starships.length && (
+              <AttributesBlock
+                data={filmData.starships}
+                classNames={['item__link']}
+                title="Starships"
+              />
+            )}
+            {!!filmData.vehicles.length && (
+              <AttributesBlock
+                data={filmData.vehicles}
+                classNames={['item__link']}
+                title="Vehicles"
+              />
+            )}
+            {!!filmData.species.length && (
+              <AttributesBlock
+                data={filmData.species}
+                classNames={['item__link']}
+                title="Species"
+              />
+            )}
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
-}
+};
 
-export default withRouter<RouterProps>(Film);
+export default Film;
