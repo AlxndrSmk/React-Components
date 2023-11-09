e0e508526543c6b877564fba5f6398ea923d7ac6
Alexander <voprosy262@gmail.com>
2023-11-10 00:57:17 +0300

    feat: add first SearchInput test

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import checker from 'vite-plugin-checker';
 export default defineConfig({
   test: {
     globals: true,
+    environment: 'jsdom',
   },
   plugins: [react(), checker({ typescript: true })],
 });
