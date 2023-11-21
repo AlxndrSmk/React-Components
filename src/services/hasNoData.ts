e747d24e105a006bc2a2f3bfee5334a65faa92ad
Alexander <voprosy262@gmail.com>
2023-11-21 23:59:14 +0300

    fix: pre-commit config

--- src/services/hasNoData.ts
+++
@@ -1,6 +0,0 @@
-const hasNoData = (value: string | undefined): boolean => {
-  const knownNoDataValues = ['unknown', '0', 'none', 'n/a'];
-  return !!value && knownNoDataValues.includes(value.toLowerCase());
-};
-
-export default hasNoData;
--- src/tests/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { HttpResponse, delay, http } from 'msw';
-import { listDataMocks } from './listDataMocks';
-
-export const handlers = [
-  http.get('https://swapi.dev/api/people', async () => {
-    await delay(150);
-    return HttpResponse.json(listDataMocks);
-  }),
-];
