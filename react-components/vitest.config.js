6b96b2e7fe473138e674ea158e9a28b7e88ee018
Alexander <voprosy262@gmail.com>
2023-11-22 00:42:09 +0300

    fix: husky pre-push and tests run

--- react-components/vitest.config.js
+++ react-components/vitest.config.js
@@ -1,13 +1,5 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      exclude: ['src/types/types.ts', 'src/constants.ts', 'src/vite-env.d.ts'],
-      all: true,
-    },
-  },
+  test: {},
 });
